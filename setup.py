8b822703da621356174c5ed5ce62b9370201602a
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2025-07-17 09:32:32 +0200

    add other package

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 setuptools.setup(
     name="hypencoder_cb",
     version="0.0.1",
-    packages=["hypencoder_cb"],
+    packages=["hypencoder_cb", "hypencoder_cb.modeling"],
     python_requires=">=3.10",
 )
